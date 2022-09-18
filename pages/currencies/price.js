046129aef93532e8dd4b3207f65c537bf7f46bb9
Rokas <rookas.rudzenskas@gmail.com>
2022-09-18 16:12:14 +0300

    Rokas

--- pages/currencies/price.js
+++ pages/currencies/price.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import CoinDetails from "../../components/CoinDetails"
-import Header from "../../components/header"
+import Header from "../../components/Header"
 
 const Price = () => {
     const [coinName, setCoinName] = useState('')
